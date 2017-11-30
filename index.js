14ae7f08fdc3c2d7f22a65aeb52ed106bd618c2c
weilyjy <184907003@qq.com>
2017-11-30 16:50:50 +0800

    last push

--- index.js
+++ index.js
@@ -1,47 +1,97 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import VueRouter from 'vue-router';
+import $ from "jquery";
+window.$ = $;
 //使用vuex
 Vue.use(Vuex);
 //使用VueRouter
 Vue.use(VueRouter);
-import xheard from './resourse/component/xheard.vue';
-import './resourse/css/heard.css';
-//import './resourse'
-
+window.bus = new Vue();
+import xheader from './resourse/component/xheader.vue';
+import xpanle from './resourse/component/xpanle.vue';
+//导航条
+import xnav from './resourse/component/xnav.vue';
+//频道
+import xchannel from './resourse/component/xchannel.vue';
+import xfooter from './resourse/component/xfooter.vue';
+import xcarousel from './resourse/component/xcarousel.vue';
+import ximages from './resourse/component/ximages.vue';
+import xloading from './resourse/component/xloading.vue';
+import xsearch from './resourse/component/xsearch.vue';
+import xdetail_header from './resourse/component/xdetail_header.vue';
+import xlogin from './resourse/component/xlogin.vue';
+import xregister from './resourse/component/xregister.vue';
+import xdetail_footer from './resourse/component/xdetail_footer.vue';
+import xdetail from "./resourse/component/xdetail.vue";
+import login from "./resourse/component/login.vue";
+import newsDetail from "./resourse/component/newsDetail.vue";
+import newsLogin from "./resourse/component/newsLogin.vue";
+import newsRegister from "./resourse/component/newsRegister.vue";
 let router = new VueRouter({
-	routes:[{
+	routes:[
+	{	path:'/',
+		component:xpanle,
+		redirect: '/index'
+	}
+	,{
 		path:"/index",
-		component:xheard,
+		component:xpanle,
+	},{
+		path:"/channel",
+		component:xchannel,
+	},{
+		path:"/images",
+		component:ximages,
+	},{
+		path:'/newsDetail',
+		component:newsDetail,
+	},{
+		path:'/newsLogin',
+		component:newsLogin,
+	},{
+		path:'/newsRegister',
+		component:newsRegister,
 	}]
 });
 let store = new Vuex.Store({
 	state:{
-		title:"vue-webpack"
+		type:'新闻',
+		bool:false,
+		newslist:[],
+		search:'',
+		title:'',
+		img:'',
+		source:'',
+		
 	}
 })
-
-new Vue({
+//主页
+let index=new Vue({
 	el:'#app',
 	router,
 	store,
 	data:{
-		name:'今日头条',
-		txt:require('./resourse/txt/data.txt'),
-		img:require('./resourse/images/img5.jpg')
+		
 	},
-//	template:require('./index.html'),
 	template:`
 	<div>
 		<router-view></router-view>
-		<xheard></xheard>
-		<p>dskan</p>
 	</div>
 	`,
 	components:{
-		xheard,
-	},
-	methods:{
+		xheader,
+		xcarousel,
+		xnav,
+		xsearch,
+		xpanle,
+		xchannel,
+		xfooter,
+		xloading,
+		xdetail,
+		newsDetail,
+		newsLogin,
 		
-	}
-})
+		
+	},
+});
